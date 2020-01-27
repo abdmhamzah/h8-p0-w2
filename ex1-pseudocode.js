7d6f2ed13a5289ce1b5d02d0d13ff1d1f662639d
abdmhamzah <iamhamzaham@gmail.com>
2020-01-27 15:05:48 +0700

    update tugas week2

---
+++ ex1-pseudocode.js
@@ -0,0 +1,75 @@
+//  NAMA: Hamzah Abdullah Mubarak
+
+// 1) NEWTON SECOND LAW
+
+// Alogaritma
+// 1. tentukan nilai "masa benda" sama dengan 600 
+// 2. tentukan nilai "percepatan benda" sama dengan 2
+// 3. tentukan "resultan gaya" sama dengan nilai masa benda dikali dengan nilai percepatan benda
+// 4. tampilkan resultan gaya
+
+// Pseudocode
+// SET "masa benda" WITH 600
+// SET "percepatan benda" WITH 2
+// SET "resultan gaya" WITH CALCULATE "masa benda" TIMES "percepatan benda"
+// DISPLAY "resultan gaya" 
+
+
+// 2) TAHUN KABISAT
+
+// Alogaritma
+// 1. tentukan nilai "tahun"
+// 2. jika "tahun" habis dibagi 4 dan tidak habis dibagi 100
+// 3. atau jika "tahun" habis dibagi 4 dan habis bagi 100 dan habis dibagi 400
+// 4. tampilkan "Ya, Tahun Kabisat"
+// 5. jika bukan
+// 6. tampilkan "Bukan Tahun Kabisat"
+
+// Pseudocode
+// SET "tahun"
+// IF "tahun" MOD 4 EQUALS 0 AND "tahun" MOD 100 NOT EQUALS 0 THEN
+//     DISPLAY "Ya, Tahun Kabisat"
+// IF ELSE "tahun" MOD 4 EQUALS 0 AND "tahun" MOD 100 EQUALS 0 AND "tahun" MOD 400 EQUALS 0 THEN
+//     DISPLAY "Ya, Tahun Kabisat"
+// ELSE 
+//     DISPLAY "Bukan Tahun Kabisat"
+// ENDIF
+
+
+// 3) LAUNDRY DAY 
+
+// Alogaritma
+// 1. tentukan jumlah seluruh pakaian sama dengan 0 
+// 2. jika seluruh pakaian jumlahnya masih dibawah 20
+// 3. maka tambahkan jumlah "seluruh pakaian" dengan 1 berulang kali sampai jumlah "seluruh pakaian" sama dengan 20 
+// 4. kemudian tampilkan "Mulai Mencuci!"
+
+// Pseudocode
+// SET "seluruh pakaian" EQUALS 0
+// WHILE "seluruh pakaian" LESS THAN 20 THEN
+//     ADD "seluruh pakaian" by 1
+// DISPLAY "Mulai Mencuci!"
+// ENDWHILE
+
+
+// 4) PERIKSA KUKU
+
+// Alogaritma
+// 1. tentukan 40 "kuku siswa" yang akan diperiksa
+// 2. jika "kuku siswa" "panjang" maka siswa akan "dihukum"
+// 3. jika tidak, maka siswa akan "dipuji"
+
+// Pseudocode
+// SET "kuku siswa"
+// IF "kuku siswa" EQUALS "panjang"
+//     DISPLAY "dihukum"
+// ELSE 
+//     DISPLAY "dipuji"
+// ENDIF
+
+// atau 
+
+// CASE "kuku siswa" OF
+//     panjang     : Print "dihukum"
+//     pendek      : Print "dipuji"
+// ENDCASE
